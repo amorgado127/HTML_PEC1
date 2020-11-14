bdb2c19b416c6d3775c04853cd53752424494d46
amorgadol <amorgado127@gmail.com>
2020-11-14 18:56:58 +0100

    Primer commit local

---
+++ src/script/datos.js
@@ -0,0 +1,67 @@
+export default data = {
+  categorias: [
+    {
+      id: 0,
+      nombre: "Novela Histórica",
+      imagen: "./img/novela_historica_large.png",
+      imagen_th: "./img/novela_historica_th.png",
+      enlace: "./categoria.html?categoria=0",
+    },
+    {
+      id: 1,
+      nombre: "Policiaca",
+      imagen: "./img/novela_negra_large.png",
+      imagen_th: "./img/novela_negra_th.png",
+      enlace: "./categoria.html?categoria=1",
+    },
+    {
+      id: 2,
+      nombre: "Clásicos",
+      imagen: "./img/ciencia_ficcion_large.png",
+      imagen_th: "./img/ciencia_ficcion_th.png",
+      enlace: "./categoria.html?categoria=2",
+    },
+    {
+      id: 3,
+      nombre: "Poesía",
+      imagen: "./img/fantasia_large.png",
+      imagen_th: "./img/fantasia_th.png",
+      enlace: "./categoria.html?categoria=3",
+    },
+  ],
+  libros: [
+    {
+      categoria: 1,
+      isbn: 9788408177081,
+      titulo: "Origen",
+      autor: "Dan Brown",
+      imagenPequeña: "./img/origen_th.jpg",
+      imagenGrande: "./img/origen_large.jpg",
+      resumen: "Robert Langdon, profesor de simbología e iconografía religiosa de la universidad de Harvard, acude al Museo Guggenheim Bilbao para asistir a un trascendental anuncio que «cambiará la faz de la ciencia para siempre». El anfitrión de la velada es Edmond Kirsch, un joven multimillonario cuyos visionarios inventos tecnológicos y audaces predicciones lo han convertido en una figura de renombre mundial. Kirsch, uno de los alumnos más brillantes de Langdon años atrás, se dispone a revelar un extraordinario descubrimiento que dará respuesta a las dos preguntas que han obsesionado a la humanidad desde el principio de los tiempos. ¿DE DÓNDE VENIMOS? ¿ADÓNDE VAMOS? Al poco tiempo de comenzar la presentación, meticulosamente orquestada por Edmond Kirsch y la directora del museo Ambra Vidal, estalla el caos para asombro de cientos de invitados y millones de espectadores en todo el mundo. Ante la inminente amenaza de que el valioso hallazgo se pierda para siempre, Langdon y Ambra deben huir desesperadamente a Barcelona e iniciar una carrera contrarreloj para localizar la críptica contraseña que les dará acceso al revolucionario secreto de Kirsch. Perseguidos por un atormentado y peligroso enemigo, Langdon y Ambra descubrirán los episodios más oscuros de la Historia y del extremismo religioso. Siguiendo un rastro de pistas compuesto por obras de arte moderno y enigmáticos símbolos, tendrán pocas horas para intentar desvelar la fascinante investigación de Kirsch... y su sobrecogedora revelación sobre el origen y el destino de la Humanidad. ORIGEN se desarrolla íntegramente en España. Barcelona, Bilbao, Madrid y Sevilla son los escenarios principales en los que transcurre la nueva aventura de Robert Langdon. De la mano del autor de El código Da Vinci, el lector recorrerá escenarios como el Monasterio de Montserrat, la Casa Milà (La Pedrera), la Sagrada Familia, el Museo Guggenheim Bilbao, el Palacio Real o la Catedral de Sevilla. Como ya sucedió con París en El código Da Vinci, con Roma en Ángeles y demonios o con Florencia en Inferno, los escenarios de las novelas de Dan Brown siempre han sido un elemento clave en sus tramas. Edición dedicada por Dan Brown.",
+      resumen_corto: "",
+    },
+    {
+      categoria: 1,
+      isbn: 9788408127413,
+      titulo: "Inferno",
+      autor: "Dan Brown",
+      imagenPequeña: "./img/inferno_th.jpg",
+      imagenGrande: "./img/inferno_large.jpg",
+      resumen:
+        "Dan Brown vuelve a los misterios históricos con su personaje favorito, el profesor de simbología Robert Langdon, con la novela Inferno, inspirada en La Divina Comedia de Dante y que saldrá a la venta el 16 de mayo. En sus bestsellers internacionales El código Da Vinci, Ángeles y demonios y El símbolo perdido, Dan Brown aunó con maestría historia, arte, códigos y símbolos. En su fascinante nuevo thriller, Brown recupera su esencia con su novela más ambiciosa hasta la fecha. En el corazón de Italia, el catedrático de Simbología de Harvard Robert Langdon se ve arrastrado a un mundo terrorífico centrado en una de las obras maestras de la Literatura más imperecederas y misteriosas de la Historia: el Infierno de Dante. Con este telón de fondo, Langdon se enfrenta a un adversario escalofriante y lidia con un acertijo ingenioso en un escenario de arte clásico, pasadizos secretos y ciencia futurista. Apoyándose en el oscuro poema épico de Dante, Langdon, en una carrera contrarreloj, busca respuestas y personas de confianza antes de que el mundo cambie irrevocablemente",
+      resumen_corto:
+        "En el corazón de Italia, el catedrático de Simbología de Harvard Robert Langdon se ve arrastrado a un mundo terrorífico centrado en una de las obras maestras de la Literatura más imperecederas y misteriosas de la Historia: el Infierno de Dante. Con este telón de fondo, Langdon se enfrenta a un adversario escalofriante y lidia con un acertijo ingenioso en un escenario de arte clásico, pasadizos secretos y ciencia futurista. Apoyándose en el oscuro poema épico de Dante, Langdon, en una carrera contrarreloj, busca respuestas y personas de confianza antes de que el mundo cambie irrevocablemente.",
+    },
+    {
+      categoria: 1,
+      isbn:9788497938891,
+      titulo: "La Biblia de barro",
+      autor: "Julia Navarro",
+      imagenPequeña: "./img/la_biblia_de_barro_th.jpg",
+      imagenGrande: "./img/la_biblia_de_barro_large.jpg",
+      resumen:
+        "En Roma, un hombre se confiesa: «Padre, me acuso de que voy a matar a un hombre...». Al mismo tiempo Clara Tannenberg, una joven arqueóloga nieta de un poderoso hombre de oscuro pasado, anuncia en el transcurso de un congreso el descubrimiento de unas tablillas que, de ser auténticas, serían la prueba científica de la existencia del patriarca Abraham: se trata de la obra de un escriba que recogió el relato del profeta sobre la creación del mundo, la confusión de las lenguas en Babel y el Diluvio universal. Una auténticaBiblia de Barro. Junto a un equipo de arqueólogos, poco antes del inicio de la última guerra del Golfo, Clara pondrá en marcha unas arriesgadas excavaciones que alientan a muchas personas a acabar con su vida y la de su abuelo: desde millonarios traficantes de arte hasta cuatro amigos que no desistirán hasta culminar una implacable venganza.     resumen_corto: En el corazón de Italia, el catedrático de Simbología de Harvard Robert Langdon se ve arrastrado a un mundo terrorífico centrado en una de las obras maestras de la Literatura más imperecederas y misteriosas de la Historia: el Infierno de Dante. Con este telón de fondo, Langdon se enfrenta a un adversario escalofriante y lidia con un acertijo ingenioso en un escenario de arte clásico, pasadizos secretos y ciencia futurista. Apoyándose en el oscuro poema épico de Dante, Langdon, en una carrera contrarreloj, busca respuestas y personas de confianza antes de que el mundo cambie irrevocablemente.",
+      resumen_corto:""  
+    },
+  ],
+};
---
+++ src/script/index.js
@@ -0,0 +1,64 @@
+//const data = require("./datos");
+
+const data = {
+  categorias: [
+    {
+      id: 0,
+      nombre: "Novela Histórica",
+      imagen: "./img/novela_historica_large.png",
+      enlace: "./categoria.html?categoria=0",
+    },
+    {
+      id: 1,
+      nombre: "Policiaca",
+      imagen: "./img/novela_negra_large.png",
+      enlace: "./categoria.html?categoria=1",
+    },
+    {
+      id: 2,
+      nombre: "Clásicos",
+      imagen: "./img/ciencia_ficcion_large.png",
+      enlace: "./categoria.html?categoria=2",
+    },
+    {
+      id: 3,
+      nombre: "Poesía",
+      imagen: "./img/fantasia_large.png",
+      enlace: "./categoria.html?categoria=3",
+    },
+  ],
+  libros: [
+    {
+      categoria: 1,
+      titulo: "El Código Da Vinci",
+      autor: "Dan Brown",
+      imagenPequeña: "",
+      imagenGrande: "",
+      resumen: "",
+    },
+    {
+      categoria: 1,
+      titulo: "",
+      autor: "",
+      imagenPequeña: "",
+      imagenGrande: "",
+      resumen: "",
+    },
+  ],
+};
+window.onload = function () {
+  const categorias = document.getElementById("container");
+  data.categorias.forEach((dato) => {
+    const div = document.createElement("div");
+    const enlaceCategoria = document.createElement("a");
+    const imagen = document.createElement("img");
+    imagen.className = "img-categoria";
+    imagen.src = dato.imagen;
+    enlaceCategoria.href = dato.enlace;
+    enlaceCategoria.appendChild(imagen);
+
+    div.appendChild(enlaceCategoria);
+    div.className = "categoria";
+    categorias.appendChild(div);
+  });
+};
