116d9877e64ed9534d881a6ffa4eb3f20f20c6fc
amorgadol <amorgado127@gmail.com>
2020-11-15 13:50:19 +0100

    commit mas libros 7

--- src/script/index.js
+++ src/script/index.js
@@ -1,37 +1,38 @@
-const data = {
-  categorias: [
-    {
-      id: 0,
-      nombre: "Novela Histórica",
-      imagen: "./img/novela_historica_large.png",
-      imagen_th: "./img/novela_historica_th.png",
-      enlace: "./categoria.html?categoria=0",
-    },
-    {
-      id: 1,
-      nombre: "Novela Negra y Thriller",
-      imagen: "./img/novela_negra_large.png",
-      imagen_th: "./img/novela_negra_th.png",
-      enlace: "./categoria.html?categoria=1",
-    },
-    {
-      id: 2,
-      nombre: "Ciencia Ficción",
-      imagen: "./img/ciencia_ficcion_large.png",
-      imagen_th: "./img/ciencia_ficcion_th.png",
-      enlace: "#",
-    },
-    {
-      id: 3,
-      nombre: "Fantasía",
-      imagen: "./img/fantasia_large.png",
-      imagen_th: "./img/fantasia_th.png",
-      enlace: "#",
-    },
-  ],
-};
 
 window.onload = function () {
+  const data = {
+    categorias: [
+      {
+        id: 0,
+        nombre: "Novela Histórica",
+        imagen: "./img/novela_historica_large.png",
+        imagen_th: "./img/novela_historica_th.png",
+        enlace: "./categoria.html?categoria=0",
+      },
+      {
+        id: 1,
+        nombre: "Novela Negra y Thriller",
+        imagen: "./img/novela_negra_large.png",
+        imagen_th: "./img/novela_negra_th.png",
+        enlace: "./categoria.html?categoria=1",
+      },
+      {
+        id: 2,
+        nombre: "Ciencia Ficción",
+        imagen: "./img/ciencia_ficcion_large.png",
+        imagen_th: "./img/ciencia_ficcion_th.png",
+        enlace: "#",
+      },
+      {
+        id: 3,
+        nombre: "Fantasía",
+        imagen: "./img/fantasia_large.png",
+        imagen_th: "./img/fantasia_th.png",
+        enlace: "#",
+      },
+    ],
+  };
+  
   const categorias = document.getElementById("container");
   data.categorias.forEach((dato) => {
     const div = document.createElement("div");
